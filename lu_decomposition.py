c14604d872600eed019dd08bf1aaa5b37f16bc4f
Jessica Cacau <jessicacacau@gmail.com>
2018-10-25 16:55:22 -0300

    Implementa a fatoração LU, sem resolver o sistema

--- lu_decomposition.py
+++ lu_decomposition.py
@@ -1,2 +1,17 @@
-def lu_decomposition():
-    pass
+def lu_decomposition(A, b):
+    # Número de linhas e colunas da matriz A
+    n = len(A)
+    
+    # Cria uma matriz L de tamanho nxn e uma matriz U que recebe o conteúdo da matriz A
+    L = [[0.0] * n for i in range(n)]
+    U = list(map(list, A))
+
+    # Faz a fatoração, alterando os valores de L e U
+    for pivot in range(n):
+        L[pivot][pivot] = 1
+        for row in range(pivot + 1, n):
+            L[row][pivot] = U[row][pivot] / U[pivot][pivot]
+            for column in range(pivot, n):
+                U[row][column] -= L[row][pivot] * U[pivot][column]
+            
+    return
