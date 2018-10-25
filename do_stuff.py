f89d32cf4eb9e6ab762c414585029737b3e9b7ff
Jessica Cacau <jessicacacau@gmail.com>
2018-10-25 15:38:39 -0300

    Corrige passagem de parâmetros da função gauss_seidel

--- do_stuff.py
+++ do_stuff.py
@@ -9,21 +9,21 @@ def do():
     b1 = []
     samples = int(input("Enter number of samples: "))
     for i in range(samples):
-        A1.append(input("Enter values of row %s in A1: " % i + 1).split(" "))
+        A1.append(input("Enter values of row %s in A1: " % str(i + 1)).split(" "))
     b1 = input("Enter values of b1: ").split(" ")
 
     # Sistema 2
     A2 = []
     b2 = []
     for i in range(samples):
-        A2.append(input("Enter values of row %s in A2: " % i + 1).split(" "))
+        A2.append(input("Enter values of row %s in A2: " % str(i + 1)).split(" "))
     b2 = input("Enter values of b2: ").split(" ")
 
     # Recebe o valor da precisão
     eps = float(input("Enter precision: "))
 
     x1 = lu_decomposition(A1, b1)
-    x2 = gauss_seidel(A2, b2)
+    x2 = gauss_seidel(A2, b2, eps)
     f = lagrange(x1, x2)
 
     # to-do:
