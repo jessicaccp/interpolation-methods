fc1b5e77ebc0ba41aa7a80955f357945edec1489
Jessica Cacau <jessicacacau@gmail.com>
2018-10-25 17:46:31 -0300

    Resolve sistemas

--- lu_decomposition.py
+++ lu_decomposition.py
@@ -10,8 +10,37 @@ def lu_decomposition(A, b):
     for pivot in range(n):
         L[pivot][pivot] = 1
         for row in range(pivot + 1, n):
-            L[row][pivot] = U[row][pivot] / U[pivot][pivot]
+            L[row][pivot] = round(U[row][pivot] / U[pivot][pivot], 5)
             for column in range(pivot, n):
-                U[row][column] -= L[row][pivot] * U[pivot][column]
-            
-    return
+                U[row][column] -= round(L[row][pivot] * U[pivot][column], 5)
+    
+    x = [0.0 for i in range(n)]
+    y = [0.0 for i in range(n)]
+
+    # Resolve L . y = b
+    for row in range(n):
+        for column in range(n):
+            if row == column:
+                y[row] += b[row]
+                break
+            y[row] -= L[row][column] * b[column]
+
+    # Resolve U . x = y
+    for row in range(n - 1, -1, -1):
+        for column in range(n - 1, -1, -1):
+            if row == column:
+                x[row] += y[row]
+                x[row] /= U[row][column]
+                break
+            x[row] -= U[row][column] * x[column]
+
+    # print(x)
+
+    print()
+    print(A)
+    print(L)
+    print(U)
+    print(y)
+    print(x)
+
+    return x
