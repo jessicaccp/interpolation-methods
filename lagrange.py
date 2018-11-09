05c80557884667c37864fbab07ebc6fbfd7f7a95
matheus-bernardino <matheusb17@hotmail.com>
2018-11-09 13:47:03 -0300

    Implementa lagrange

--- lagrange.py
+++ lagrange.py
@@ -1,12 +1,20 @@
 def lagrange(x, y):
-    pass
-    # n = len(x)
-    # midpoint = (x[n-1] - x[0]) / 2
+    n = len(x)
+    midpoint = x[0] + ((x[n-1] - x[0]) / 2)
+    
+    diference = 10000
+    for i in range(1, n-1):
+        if abs(midpoint - x[i]) < diference:
+            if midpoint - x[i] < 0:
+                i0, i1 = i, i-1
+            else:
+                i0, i1 = i, i+1
+            
+            diference = abs(midpoint - x[i])
+    
+    factor1 = y[i0]*((midpoint - x[i1]) / (x[i0] - x[i1]))
+    factor2 = y[i1]*((midpoint - x[i0]) / (x[i1] - x[i0]))
 
-    # pn = 0
-    # for i in range(n):
-    #     lk = 0
-    #     for j in range(n):
-    #         midpoint
+    p = factor1 + factor2
 
-    #     p += ny[k]
+    return p
